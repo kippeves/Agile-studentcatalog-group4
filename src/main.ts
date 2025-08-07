0c8744338c6ab1c5e1d7a62a86c3e5a3e1ef4bee
Christoffer <nordin.christoffer@gmail.com>
2025-08-07 14:55:20 +0200

    Checkbox listener and toggleStudentActive function.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 
 import { getRequiredElement } from "./utils/domHelpers.js";
 import { Student } from "./models/student.js";
-import { loadStudents } from "./services/student.js";
+import { loadStudents, saveStudents } from "./services/student.js";
 import { createStudentListItem } from "./ui/createStudentListItem.js";
 
 const listContainer = getRequiredElement("#student-ul") as HTMLUListElement;
 
 renderStudentList();
+
+listContainer.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+    switch (target.tagName) {
+        case "INPUT":
+            toggleStudentActive(target);
+            break;
+    }
+});
 
 function renderStudentList(): void {
     listContainer.textContent = "";
@@ -18,4 +27,22 @@ function renderStudentList(): void {
     const item = document.createElement("li");
     item.textContent = "Listan är tom...";
     listContainer.appendChild(item);
+}
+
+function toggleStudentActive(target: HTMLElement): void {
+    const input = target as HTMLInputElement;
+    const val = input.closest("li")?.dataset["studentId"];
+
+    if (val === undefined || isNaN(parseInt(val)))
+        return;
+
+    const students: Student[] = loadStudents();
+    const index = students.findIndex(item => item.id === parseInt(val));
+    if (index < 0 || students[index] === undefined)
+        return;
+    let student = students[index];
+    student.isActive = input.checked;
+
+    students[index] = student;
+    //
 }
