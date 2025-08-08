0833239556a34b640d6a4248339e41e14a2aef44
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-08-08 09:21:55 +0200

    Add form click handler
    Add form handler for all clicks
    Add handleDeletion logic
    Move renderStudents and createStudentListItem into studentList file

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,50 @@
-
+import { BUTTON_ACTIONS } from "./config.js";
+import { checkIfDataIsInitialized, deleteStudent } from "./services/student.js";
+import { renderStudentList } from "./ui/studentList.js";
 import { getRequiredElement } from "./utils/domHelpers.js";
-import { checkIfDataIsInitialized} from "./services/student.js";
-import { createStudentListItem } from "./ui/createStudentListItem.js";
-import { load } from "./data/student.js";
 
-const listContainer = getRequiredElement("#student-ul") as HTMLUListElement;
-
-checkIfDataIsInitialized();
-renderStudentList();
-
-function renderStudentList(): void {
-    listContainer.textContent = "";
-    // Placeholder array
-    const students = load();
-    if (!students)
-        return;
-    students.forEach((item) => listContainer.appendChild(createStudentListItem(item)));
-    if (students.length > 0)
-        return;
-    const item = document.createElement("li");
-    item.textContent = "Listan är tom...";
-    listContainer.appendChild(item);
+function handleStudentDeletion(
+  listContainer: HTMLElement,
+  studentId: number
+): void {
+  deleteStudent(studentId);
+  renderStudentList(listContainer);
 }
+
+function handleStudentListClick(e: MouseEvent): void {
+  const container = e.currentTarget as HTMLLIElement;
+  const target = e.target as HTMLElement;
+
+  // Find parent button of the target
+  const btn = target.closest<HTMLElement>("[data-action]");
+  if (!btn) return;
+
+  // Find parent list item of the button
+  const listItem = btn.closest<HTMLLIElement>("li[data-student-id]");
+  if (!listItem)
+    throw new Error(`No list item found with id for button ${btn}`);
+
+  const studentId = Number(listItem.dataset["studentId"]);
+  const action = btn.dataset["action"];
+
+  // Call appropriate function for action
+  switch (action) {
+    case BUTTON_ACTIONS.DELETE_STUDENT:
+      handleStudentDeletion(container, studentId);
+      break;
+    case BUTTON_ACTIONS.TOGGLE_ACTIVE:
+      break;
+  }
+}
+
+function intialisePage(): void {
+  const listContainer = getRequiredElement<HTMLUListElement>("#student-ul");
+
+  listContainer.addEventListener("click", handleStudentListClick);
+
+  checkIfDataIsInitialized();
+  renderStudentList(listContainer);
+}
+
+// Entry point
+document.addEventListener("DOMContentLoaded", intialisePage);
---
+++ src/ui/studentList.ts
@@ -0,0 +1,42 @@
+import { BUTTON_ACTIONS, ICON_PATHS } from "../config.js";
+import { load } from "../data/student.js";
+import { Student } from "../models/student.js";
+
+function createStudentListItem(student: Student): HTMLLIElement {
+  const isChecked = student.isActive;
+
+  const li = document.createElement("li");
+  li.dataset["studentId"] = student.id.toString();
+
+  li.innerHTML = `
+    <span class="student-name">${student.name}</span>
+    <span class="student-age">${student.age}</span>
+    <label class="visually-hidden">
+      Active
+      <input type="checkbox" data-action=${
+        BUTTON_ACTIONS.TOGGLE_ACTIVE
+      } name="isActiveInput" ${isChecked ? "checked" : ""}>
+    </lnpx tscabel>
+    <button type="button" data-action=${
+      BUTTON_ACTIONS.DELETE_STUDENT
+    } aria-label="Delete student">
+        <img src=${ICON_PATHS.DELETE_TRASH} alt="" aria-hidden="true">
+    </button>
+  `;
+
+  return li;
+}
+
+export function renderStudentList(listContainer: HTMLElement): void {
+  listContainer.textContent = "";
+  // Placeholder array
+  const students = load();
+  if (!students) return;
+  students.forEach((item) =>
+    listContainer.appendChild(createStudentListItem(item))
+  );
+  if (students.length > 0) return;
+  const item = document.createElement("li");
+  item.textContent = "Listan är tom...";
+  listContainer.appendChild(item);
+}
