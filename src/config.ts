99e66795dfb15f059b3c12075badde994cc85cc8
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-08-08 09:07:35 +0200

    Add button data-action types to config

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,8 @@ const iconsRoot = "./assets/icons/";
 export const ICON_PATHS = {
   DELETE_TRASH: `${iconsRoot}delete_trash.png`,
 };
+
+export const BUTTON_ACTIONS = {
+  DELETE_STUDENT: "delete-student",
+  TOGGLE_ACTIVE: "toggle-active",
+};
